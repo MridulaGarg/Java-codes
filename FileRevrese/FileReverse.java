71ef03d0f00748db1d2b3b18bc1438f93dbb6b13
Mridula Garg <mgarg425@gmail.com>
2018-03-17 00:59:34 +0530

    Reversing a File and storing in another file

---
+++ FileRevrese/FileReverse.java
@@ -0,0 +1,66 @@
+import java.io.*;
+
+import java.util.*;
+
+
+
+public class FileReverse
+
+{
+
+	public static void main(String[] args)
+
+	{
+		try
+
+		{
+
+			FileReader f1 = new FileReader(args[0]);
+
+			Scanner in = new Scanner(f1);
+
+			FileWriter f2 = new FileWriter(args[1]);
+
+			String str, s;
+
+			StringTokenizer t;
+
+			Formatter f = new Formatter(f2);
+
+
+			while(in.hasNext())
+
+			{
+
+				s = " ";
+				str = in.nextLine();
+
+				t = new StringTokenizer(str);
+
+				while(t.hasMoreTokens())
+
+					s = t.nextToken() + " " + s;
+				f.format("%s\n", s);
+
+			}
+
+			f.close();
+
+			f1.close();
+
+			f2.close();
+
+		}
+
+
+		catch(IOException e)
+
+		{
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
